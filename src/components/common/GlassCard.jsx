c7493b339a4870d56fd29b348a6974abb7859078
allexl1 <ptjnj6bmkv@privaterelay.appleid.com>
2026-08-12 00:16:12 +0300

    Update GlassCard.jsx

--- src/components/common/GlassCard.jsx
+++ src/components/common/GlassCard.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
 
-export default function GlassCard({
-  children,
-  className = '',
-  interactive = false,
-  onClick
-}) {
+export default function GlassCard({ children, className = '', interactive = false, onClick }) {
   const baseClass = interactive ? 'liquid-glass-interactive cursor-pointer' : 'liquid-glass';
-
   return (
     <div
       onClick={onClick}
-      className={`${baseClass} rounded-2xl p-4 ${className}`}
+      className={`${baseClass} rounded-2xl p-4 transition-all duration-200 ${className}`}
     >
       {children}
     </div>
