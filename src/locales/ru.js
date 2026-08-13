16736e371fc0be1638a1d8879a1d586f23271dfa
allexl1 <ptjnj6bmkv@privaterelay.appleid.com>
2026-08-13 22:39:18 +0300

    Create ru.js

---
+++ src/locales/ru.js
@@ -0,0 +1,19 @@
+export const ru = {
+  nav: {
+    home: "Главная",
+    schedule: "Расписание",
+    teachers: "Преподаватели",
+    exams: "Экзамены",
+    settings: "Настройки"
+  },
+  settings: {
+    title: "Настройки",
+    subtitle: "Параметры приложения",
+    language: "Язык интерфейса",
+    group: "Учебная группа",
+    theme: "Тема оформления",
+    themeSystem: "Системная",
+    themeLight: "Светлая",
+    themeDark: "Тёмная"
+  }
+};
