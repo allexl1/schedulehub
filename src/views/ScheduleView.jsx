d22c6411664dceda5cd9980cd4feb3211df3b8ad
allexl1 <ptjnj6bmkv@privaterelay.appleid.com>
2026-08-20 02:07:37 +0300

    Update ScheduleView.jsx

--- src/views/ScheduleView.jsx
+++ src/views/ScheduleView.jsx
@@ -680,31 +680,32 @@ export default function ScheduleView({
                 })
               );
 
-            const items = [
-              ...lessons,
-              ...personal.map(
-                event => ({
-                  ...event,
-                  status:
-                    sameDate(
-                      section.date,
-                      now
-                    )
-                      ? 'upcoming'
-                      : section.date <
-                          now
-                        ? 'past'
-                        : 'upcoming'
-                })
-              ].sort(
-                (a, b) =>
-                  parseStartTimeInMinutes(
-                    a.time
-                  ) -
-                  parseStartTimeInMinutes(
-                    b.time
-                  )
-              );
+  const items = [
+  ...lessons,
+  ...personal.map(
+    event => ({
+      ...event,
+      status:
+        sameDate(
+          section.date,
+          now
+        )
+          ? 'upcoming'
+          : section.date <
+              now
+            ? 'past'
+            : 'upcoming'
+    })
+  )
+].sort(
+  (a, b) =>
+    parseStartTimeInMinutes(
+      a.time
+    ) -
+    parseStartTimeInMinutes(
+      b.time
+    )
+);
 
             return {
               ...section,
